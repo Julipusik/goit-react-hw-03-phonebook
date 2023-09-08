ce0100b0e1899ef6c1eae0d009e69446fc342751
Julipusik <yuliuaboyko@gmail.com>
2023-09-08 13:39:58 +0300

    hw 3 task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,19 @@ export class App extends React.Component {
     filter: '',
   }
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({contacts: JSON.parse(savedContacts)})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = newContact => {
     const isExist = this.state.contacts.find(
       contact => contact.name === newContact.name
